0480dd0fbf0a708996ccfd25cddc931234bc1a19
Alex Dearden <alex.dearden@farepilot.com>
2018-03-30 17:28:48 +0100

    Removed Cartography pod

--- Pods/Target Support Files/Pods-Quiz/Pods-Quiz-frameworks.sh
+++ Pods/Target Support Files/Pods-Quiz/Pods-Quiz-frameworks.sh
@@ -102,12 +102,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/Cartography/Cartography.framework"
   install_framework "${PODS_ROOT}/Reveal-SDK/RevealServer-10/iOS/RevealServer.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyAttributes/SwiftyAttributes.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/Cartography/Cartography.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyAttributes/SwiftyAttributes.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
